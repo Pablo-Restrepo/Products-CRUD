26c08b7da5234675f9f8789aab14c28c6051d79d
Pablo736 <restrepo736@gmail.com>
2023-07-10 16:07:40 -0500

    Project

---
+++ src/main/java/com/pablo736/productscrud/ProductscrudApplication.java
@@ -0,0 +1,13 @@
+package com.pablo736.productscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductscrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductscrudApplication.class, args);
+	}
+
+}
