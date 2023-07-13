2baf62289eced526842173df62ae1e725fc7d130
Pablo736 <restrepo736@gmail.com>
2023-07-13 16:52:43 -0500

    Refactored project

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
+# sh deploy.sh
+
 echo "Deteniendo y eliminando los contenedores y volúmenes..."
 docker-compose down -v
 
-echo "Compilando y empaquetando el proyecto productscrud..."
-mvn clean package -f pom.xml
+echo "Compilando y empaquetando el proyecto productscrud-backend..."
+mvn clean package -f productscrud-backend/pom.xml
 
 echo "Creando y ejecutando los servicios definidos en docker-compose.yml..."
 docker-compose up --build -d
