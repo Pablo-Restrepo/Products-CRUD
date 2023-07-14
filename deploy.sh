77ab352c276741cedf39f6831f9538ec8ece3fee
Pablo736 <restrepo736@gmail.com>
2023-07-14 16:21:30 -0500

    Header and Card

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "Deteniendo y eliminando los contenedores y volúmenes..."
 docker-compose down -v
 
 echo "Compilando y empaquetando el proyecto productscrud-backend..."
-mvn clean package -f productscrud-backend/pom.xml
+#mvn clean package -f productscrud-backend/pom.xml
 
 echo "Creando y ejecutando los servicios definidos en docker-compose.yml..."
 docker-compose up --build -d
