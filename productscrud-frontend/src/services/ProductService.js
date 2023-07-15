27890317bfd81985b9fef5c509c35f33aa52226e
Pablo736 <restrepo736@gmail.com>
2023-07-15 10:27:11 -0500

    CRUD methods

--- productscrud-frontend/src/services/ProductService.js
+++ productscrud-frontend/src/services/ProductService.js
@@ -6,6 +6,22 @@ class ProductService {
     getAllProducts() {
         return axios.get(PRODUCT_BASE_REST_API_URL);
     }
+
+    getProductById(productId) {
+        return axios.get(`${PRODUCT_BASE_REST_API_URL}/${productId}`);
+    }
+
+    createProduct(product) {
+        return axios.post(PRODUCT_BASE_REST_API_URL, product);
+    }
+
+    updateProduct(productId, product) {
+        return axios.put(`${PRODUCT_BASE_REST_API_URL}/${productId}`, product);
+    }
+
+    deleteProduct(productId) {
+        return axios.delete(`${PRODUCT_BASE_REST_API_URL}/${productId}`);
+    }
 }
 
 export default new ProductService();
